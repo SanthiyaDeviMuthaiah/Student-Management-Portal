fb4fbfae5101f7507c2e1907e5ab1bd95ed44da1
Muthaiah <santhiyadevi.muthaiah@sky.uk>
2025-12-19 20:29:13 +0530

    Added database folder and SQL script

---
+++ database/school_db.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE school_db;
+USE school_db;
+
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    phone VARCHAR(15),
+    course VARCHAR(50),
+    dob DATE,
+    gender VARCHAR(10)
+);
+
+
+INSERT INTO students VALUES
+(1,'John','Doe','john@mail.com','1234567890','Science','2002-05-10','Male');
+
