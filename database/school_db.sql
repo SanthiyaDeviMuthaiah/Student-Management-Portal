6526ecd1a877eb0c92f70cb833910dfbbd78615f
Muthaiah <santhiyadevi.muthaiah@sky.uk>
2025-12-20 18:56:48 +0530

    Updated sample student data

--- database/school_db.sql
+++ database/school_db.sql
@@ -16,3 +16,10 @@ CREATE TABLE students (
 INSERT INTO students VALUES
 (1,'John','Doe','john@mail.com','1234567890','Science','2002-05-10','Male');
 
+USE school_db;
+
+INSERT INTO students 
+(student_id, first_name, last_name, email, phone, course, dob, gender)
+VALUES
+(3, 'Amit', 'Shah', 'amit@gmail.com', '9999999999', 'Science', '2001-08-12', 'Male');
+
